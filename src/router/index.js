15178c6c1cda6e09c8d0ebc30edcb6f3b08418da
archiecheng <archiechengice@outlook.com>
2024-02-06 14:59:11 -0800

    二级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,31 @@
 // 路由配置
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
+import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router-dom";
 
 import { AuthRoute } from '@/components/AuthRoute'
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element: <AuthRoute><Layout /></AuthRoute>
+        element: <AuthRoute><Layout /></AuthRoute>,
+        children: [
+            {
+                path:'home',
+                element: <Home />
+            },
+            {
+                path:'article',
+                element: <Article />
+            },
+            {
+                path:'publish',
+                element: <Publish />
+            }
+        ]
     },
     {
         path:'/login',
