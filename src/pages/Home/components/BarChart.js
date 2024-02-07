75c304b262fc8e449cd7d3be086594e4a31d04f9
archiecheng <archiechengice@outlook.com>
2024-02-06 16:25:05 -0800

    组件封装问题修复

--- src/pages/Home/components/BarChart.js
+++ src/pages/Home/components/BarChart.js
@@ -1,44 +1,43 @@
-import * as echarts from "echarts";
-import { useEffect } from "react";
 // 柱状图组件
-
-// 1. 把功能代码都放到组件中
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
+// 1. 把功能代码都放到这个组件中
 // 2. 把可变的部分抽象成prop参数
 
-const BarChart = ({title}) => {
+const BarChart = ({ title }) => {
+  const chartRef = useRef(null)
   useEffect(() => {
-    // 保证 dom 可用, 才进行图表的渲染
-    // 1. 获取要渲染图表的DOM节点
-    const chartDom = document.getElementById("main");
+    // 保证dom可用 才进行图表的渲染
+    // 1. 获取渲染图表的dom节点
+    const chartDom = chartRef.current
 
     // 2. 图表初始化生成图表实例对象
-    const myChart = echarts.init(chartDom);
+    const myChart = echarts.init(chartDom)
 
-    // 3. 图表参数
+    // 3. 准备图表参数
     const option = {
       title: {
-        text: title,
+        text: title
       },
       xAxis: {
-        type: "category",
-        data: ["Vue", "React", "Angular"],
+        type: 'category',
+        data: ['Vue', 'React', 'Angular']
       },
       yAxis: {
-        type: "value",
+        type: 'value'
       },
       series: [
         {
           data: [10, 40, 70],
-          type: "bar",
-        },
-      ],
-    };
-
+          type: 'bar'
+        }
+      ]
+    }
     // 4. 使用图表参数完成图表的渲染
-    option && myChart.setOption(option);
-  }, [title]);
-  return <div id="main" style={{ width: "500px", height: "400px" }}></div>;
-};
+    option && myChart.setOption(option)
 
+  }, [title])
+  return <div ref={chartRef} style={{ width: '500px', height: '400px' }}></div>
+}
 
 export default BarChart
