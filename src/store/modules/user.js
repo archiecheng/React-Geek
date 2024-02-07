407e8692ca75d49234f42981be2fe1d4ab947c7a
archiecheng <archiechengice@outlook.com>
2024-02-06 22:36:49 -0800

    发布文章-接入富文本编辑器

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,59 +1,58 @@
-// 放置和用户相关的状态管理
+// 和用户相关的状态管理
 
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 import { request, setToken as _setToken, getToken, removeToken } from '@/utils'
-import { loginAPI, getProfileAPI } from "@/apis/user";
+// import { loginAPI, getProfileAPI } from '@/apis/user'
+
 const userStore = createSlice({
-    name:'user',
-    // 数据状态
-    initialState: {
-        token: getToken() || '',
-        userInfo: {}
+  name: "user",
+  // 数据状态
+  initialState: {
+    token: getToken() || '',
+    userInfo: {}
+  },
+  // 同步修改方法
+  reducers: {
+    setToken (state, action) {
+      state.token = action.payload
+      _setToken(action.payload)
     },
-    // 同步修改方法
-    reducers: {
-        setToken(state, action) {
-            state.token = action.payload
-            // localStorage存一份
-            _setToken(action.payload)
-        },
-        setUserInfo(state, action) {
-            state.userInfo = action.payload
-        },
-        clearUserInfo(state) {
-            state.token = ''
-            state.userInfo = {}
-            removeToken()
-        }
+    setUserInfo (state, action) {
+      state.userInfo = action.payload
+    },
+    clearUserInfo (state) {
+      state.token = ''
+      state.userInfo = {}
+      removeToken()
     }
+  }
 })
+
 
 // 解构出actionCreater
 
 const { setToken, setUserInfo, clearUserInfo } = userStore.actions
 
-// 获取 reducer 函数
+// 获取reducer函数
 
 const userReducer = userStore.reducer
 
-// 异步方法 完成登录获取token
-
+// 登录获取token异步方法封装
 const fetchLogin = (loginForm) => {
-    return async (dispatch) => {
-        // 1. 发送异步请求
-        const res = await loginAPI(loginForm)
-        // 2. 提交同步方法action进行token的存入
-        dispatch(setToken(res.data.token))
-    }
+  return async (dispatch) => {
+    const res = await request.post('/authorizations', loginForm)
+    dispatch(setToken(res.data.token))
+  }
 }
+
 // 获取个人用户信息异步方法
 const fetchUserInfo = () => {
-    return async (dispatch) => {
-        const res = await getProfileAPI()
-        dispatch(setUserInfo(res.data))
-    }
+  return async (dispatch) => {
+    const res = await request.get('/user/profile')
+    dispatch(setUserInfo(res.data))
+  }
 }
 
-export { fetchLogin, fetchUserInfo, setToken, clearUserInfo }
+export { fetchLogin, fetchUserInfo, clearUserInfo }
 
 export default userReducer
